9380db692f61ca5238fcd4e2ae645524ca1cb36b
Alex Lam <alam.seraph@gmail.com>
2022-12-09 07:57:53 -0800

    initial

---
+++ practice1/array1.py
@@ -0,0 +1,7 @@
+arr = []
+for i in range (4):
+    answer = input("Enter a word: ")
+    arr.append(answer)
+
+number = int(input("Enter a number"))
+print(arr[number])
---
+++ practice1/sum.py
@@ -0,0 +1,6 @@
+sum = 0
+prices = [10, 20, 40, 60, 90]
+
+for i in prices:
+    sum = sum + i
+print(sum)
